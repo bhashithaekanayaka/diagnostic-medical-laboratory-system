dcefc053cacb0463b167cd8c6aa1567cb78269e6
bhashithaekanayaka <bhashithaekanayaka@gmail.com>
2026-02-15 15:36:56 +0530

    refactor: clean up App component and add new files for structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div class='bg-black h-screen'>
-        <h1 className='md:text-7xl text-2xl items-center text-center underline text-[#a1a1a1]'>Welcome to React!</h1>
-    <p>gfxggcgh</p>
-        <p className='bg-green-800 p-9 m-2.5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quia earum corporis blanditiis? Eligendi suscipit ad error vero. Dignissimos illum ipsum reiciendis provident suscipit eius commodi possimus. Laudantium, reiciendis saepe.</p>
+    <div>
+      
     </div>
   )
 }
